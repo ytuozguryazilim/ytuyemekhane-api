27e3cc3b27b4991b26898d7e9bfc47725aedb707
pleycpl <h4rvey@protonmail.com>
2018-01-14 14:08:19 +0000

    sendJson fonksiyonu eklendi. Ve FoodListByCurrentTime router'i guncellendi.

--- routers/routers.go
+++ routers/routers.go
@@ -1,19 +1,49 @@
 package routers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/GnuYtuce/ytuyemekhane-api/crawler"
+	"github.com/GnuYtuce/ytuyemekhane-api/models"
+	"github.com/GnuYtuce/ytuyemekhane-api/util"
 	"github.com/julienschmidt/httprouter"
 )
 
 var (
 	// YtuYemekhaneURL : yildiz teknik yemekhane listersini cekilecegi site.
-	YtuYemekhaneURL = "http://www.sks.yildiz.edu.tr/yemekmenu/"
+	YtuYemekhaneURL = "http://www.sks.yildiz.edu.tr/yemekmenu"
 )
 
 // FoodListByCurrentTime : suanki zamana gore yemek listesi donulecek.
 func FoodListByCurrentTime(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.Write([]byte("FoodListByCurrentTime function"))
+	/*
+		now := time.Now()
+		date := models.Date{
+			Day:   now.Day(),
+			Month: int(now.Month()),
+			Year:  now.Year(),
+		}
+	*/
+	date := models.Date{
+		Day:   18,
+		Month: 1,
+		Year:  2018,
+	}
+	fmt.Println(date)
+	url := YtuYemekhaneURL + util.CreateVirtualPATH(date.Month, date.Year)
+	menus, _ := crawler.Crawl(url)
+	for _, menu := range menus {
+		fmt.Println("##########")
+		menu.Print()
+		fmt.Println(date.ToString())
+		if menu.Date.ToString() == date.ToString() {
+			sendJSON(w, menu)
+			return
+		}
+	}
+	sendJSON(w, models.Menu{})
 }
 
 // FoodListByCertainYear : belli bir yila gore yemek listesi donulecek.
@@ -29,4 +59,12 @@ func FoodListByCertainYearAndMonth(w http.ResponseWriter, r *http.Request, p htt
 // FoodListByCertainTime : belli bir zamana gore yemek listesi donulecek.
 func FoodListByCertainTime(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Write([]byte("FoodListByCertainTime function"))
+}
+
+// sendJSON :
+func sendJSON(w http.ResponseWriter, data models.Menu) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	jContent, _ := json.MarshalIndent(data, "", " ")
+	w.Write(jContent)
 }
