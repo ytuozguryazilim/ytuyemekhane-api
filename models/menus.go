cecb2fccc7fc5a9065e0dbbc7a736950ae92dc80
pleycpl <h4rvey@protonmail.com>
2018-01-14 16:07:59 +0000

    SearchMenuByDate method'u eklendi.

--- models/menus.go
+++ models/menus.go
@@ -1,4 +1,23 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Menus :
 type Menus []Menu
+
+// SearchMenuByDate :
+func (ms Menus) SearchMenuByDate(d Date) (Menu, error) {
+	fmt.Println(d.ToString())
+	for _, m := range ms {
+		fmt.Println("##########")
+		m.Print()
+		fmt.Println(d.ToString())
+		if m.Date.ToString() == d.ToString() {
+			return m, nil
+		}
+	}
+	return Menu{}, errors.New("Bu tarihe ait herhangi bir menu yok")
+}
